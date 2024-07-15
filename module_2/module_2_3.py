4875c1b5f10fc5e3ed90a6b68c1e18e65a00f249
Nazim74 <30327515+nazim74@users.noreply.github.com>
2024-07-15 08:43:28 +0300

    completing the task

--- module_2/module_2_3.py
+++ module_2/module_2_3.py
@@ -1,11 +1,11 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 initial_value = 0
 print("my_list =", my_list)
-while len(my_list) > initial_value:
-    number_my_list = my_list[initial_value]
-    if number_my_list < 0:
+while len(my_list) > initial_value: #проверяем что количество значений в списке больше чем 0,1,2 и до 13. Как только len(my_list) станет равен  initial_value цикл остановиться
+    number_of_my_list = my_list[initial_value] #создаем переменную равную конкретному  значению в списке, первое значение (42)
+    if number_of_my_list < 0: #проверяем что значение отрицательное
         break
-    if number_my_list > 0:
-        print(number_my_list)
-        initial_value = initial_value + 1
-        continue
+    if number_of_my_list > 0: #проверяем что значение положительное
+        print(number_of_my_list)
+    initial_value = initial_value + 1 #берем сделующее значение из списка
+
