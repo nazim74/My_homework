433a553f443c059a12c926a439aeb04610ed0d31
Nazim74 <30327515+nazim74@users.noreply.github.com>
2024-07-16 11:19:09 +0300

    completing the task

--- module_2/module_2_5.py
+++ module_2/module_2_5.py
@@ -1,8 +1,12 @@
-def get_matrix(n, m, value):
+#def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        string = []
-        for j in range(m):
-            string.append(value)
-        matrix.append(string)
+       string = []
+       for j in range(m):
+           string.append(value)
+      matrix.append(string)
+
+
+
+
 
