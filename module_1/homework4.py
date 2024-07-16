db389f42c91d24388f09418e1e74630b45e9c093
Nazim74 <30327515+nazim74@users.noreply.github.com>
2024-07-16 12:00:15 +0300

    completing the task

---
+++ module_1/homework4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите несколько слов: ')
+print('количество введенных символов, с учетом пробелов, равно: ', len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string = my_string.replace(' ', '')
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
+
---
+++ module_1/homework6.py
@@ -0,0 +1,17 @@
+my_dict = {'Nazim': 1974, 'Maxim': 1980, 'Alex': 2001}
+print('Dict:', my_dict)
+print('Existing value: ', my_dict['Nazim'])
+print('Not existing value: ', my_dict.get('Boris'))
+my_dict['Rick'] = 1967
+my_dict['Morty'] = 2011
+deleted_value = my_dict.pop('Morty')
+print('Deleted value: ', deleted_value)
+print(my_dict)
+
+my_set = {'one', 2, 3, False, 'one', 2, 3, True, False}
+print('Set: ', my_set)
+my_set.add('new')
+my_set.add(777)
+my_set.remove(False)
+list_ = [2, 3, 2, 2]
+print('Modified set: ', my_set)
