210fee5b0e57ab522bf136c8438d48c016dbb4c1
nazim <3037223@gmail.com>
2024-07-20 01:39:50 +0300

    complate

---
+++ module_3/module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    for i in list_to_search:
+        if string_lower == i.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
+
