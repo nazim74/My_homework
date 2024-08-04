629042ae4aab426b3d405865ccc3e8d90ca9ce02
Nazim <3037223@gmail.com>
2024-08-04 13:31:46 +0300

    complete

--- module_5/module_5_2.py
+++ module_5/module_5_2.py
@@ -13,7 +13,7 @@ class House:
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+        return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
