80c25edcde5a305c482adf7aa6791c63336ee73e
Nazim <3037223@gmail.com>
2024-10-06 00:04:46 +0300

    complete

---
+++ module_10/module_10_1.py
@@ -0,0 +1,41 @@
+import threading
+from time import sleep, time
+from datetime import timedelta
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as f:
+        for i in range(1, word_count + 1):
+            f.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+
+start_time = time()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+end_time = time()
+print(f"Время работы функций: {timedelta(seconds=end_time - start_time)}")
+
+start_time = time()
+
+thread1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
+thread2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
+thread3 = threading.Thread(target=write_words, args=(200, 'example7.txt'))
+thread4 = threading.Thread(target=write_words, args=(100, 'example8.txt'))
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+
+end_time = time()
+print(f"Время работы потоков: {timedelta(seconds=end_time - start_time)}")
