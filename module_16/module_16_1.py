60128ace2d05bf6146def756d18caaeaa35081eb
Nazim <3037223@gmail.com>
2024-12-10 23:39:34 +0300

    complete

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -7,19 +7,19 @@ app = FastAPI()
 # Маршрут главной страницы
 @app.get("/")
 async def main_page():
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 # Маршрут страницы администратора
 @app.get("/user/admin")
 async def admin_page():
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 # Маршрут для страниц пользователей по ID
 @app.get("/user/{user_id}")
 async def user_page(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
-# Маршрут информация о пользователе
+# Маршрут для информации о пользователе
 @app.get("/user")
 async def user_info(username: Optional[str] = "Неизвестно", age: Optional[int] = 0):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}."
